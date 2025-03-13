0e1d8c4ba6fb023ae50cc58b1b059ea96481566f
hza1128 <141570215+hza1128@users.noreply.github.com>
2025-03-13 15:10:23 +0800

    Update ip_addr_r5s.sh

--- patches_my/ip_addr_r5s.sh
+++ patches_my/ip_addr_r5s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 uci set network.lan.proto='static'
-uci set network.lan.ipaddr='192.168.5.1'
+uci set network.lan.ipaddr='192.168.10.11'
 uci set network.lan.netmask='255.255.255.0'
-uci set network.lan.gateway='192.168.5.1'
-uci set network.lan.dns='192.168.5.1'
+uci set network.lan.gateway='192.168.10.1'
+uci set network.lan.dns='192.168.10.1'
 uci commit network
